a814e40f3c0ee81ed578d0291f243a1c4803b0a2
mydeenmk <a.mydeenmohammed@gmail.com>
2024-03-29 15:56:53 +0530

    main

---
+++ routes/CURL Command to verify phone number.sh
@@ -0,0 +1,18 @@
+CURL Command to verify phone number
+
+curl -X POST "https://verify.twilio.com/v2/Services/VAc3bd6fd3a489cd7f31bca92385800155/Verifications" \
+  --data-urlencode "To=+919790550158" \
+  --data-urlencode "Channel=sms" \
+  -u "AC4f5f38aca4ca5ff4ad7e560e2dc14ba7:71171031dedec9290004e7a221b71fb0"
+
+echo
+echo -n "Please enter the OTP:"
+read OTP_CODE
+
+curl -X POST "https://verify.twilio.com/v2/Services/VAc3bd6fd3a489cd7f31bca92385800155/VerificationCheck" \
+  --data-urlencode "To=+919790550158" \
+  --data-urlencode "Code=$OTP_CODE" \
+  -u "AC4f5f38aca4ca5ff4ad7e560e2dc14ba7:71171031dedec9290004e7a221b71fb0"
+
+
+  +12055767129
