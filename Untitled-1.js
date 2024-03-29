a58871ecc57989051d3be7d369b60fc472b3e535
mydeenmk <a.mydeenmohammed@gmail.com>
2024-03-29 16:06:06 +0530

    main

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,26 +1,26 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Set environment variables for your credentials
-// Read more at http://twil.io/secure
-const accountSid = "AC4f5f38aca4ca5ff4ad7e560e2dc14ba7";
-const authToken = "71171031dedec9290004e7a221b71fb0";
-const verifySid = "VAc3bd6fd3a489cd7f31bca92385800155";
-const client = require("twilio")(accountSid, authToken);
+// // Download the helper library from https://www.twilio.com/docs/node/install
+// // Set environment variables for your credentials
+// // Read more at http://twil.io/secure
+// const accountSid = "AC4f5f38aca4ca5ff4ad7e560e2dc14ba7";
+// const authToken = "71171031dedec9290004e7a221b71fb0";
+// const verifySid = "VAc3bd6fd3a489cd7f31bca92385800155";
+// const client = require("twilio")(accountSid, authToken);
 
-client.verify.v2
-  .services(verifySid)
-  .verifications.create({ to: "+919790550158", channel: "sms" })
-  .then((verification) => console.log(verification.status))
-  .then(() => {
-    const readline = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+// client.verify.v2
+//   .services(verifySid)
+//   .verifications.create({ to: "+919790550158", channel: "sms" })
+//   .then((verification) => console.log(verification.status))
+//   .then(() => {
+//     const readline = require("readline").createInterface({
+//       input: process.stdin,
+//       output: process.stdout,
+//     });
     
-    readline.question("Please enter the OTP:", (otpCode) => {
-      client.verify.v2
-        .services(verifySid)
-        .verificationChecks.create({ to: "+919790550158", code: otpCode })
-        .then((verification_check) => console.log(verification_check.status))
-        .then(() => readline.close());
-    });
-  });
+//     readline.question("Please enter the OTP:", (otpCode) => {
+//       client.verify.v2
+//         .services(verifySid)
+//         .verificationChecks.create({ to: "+919790550158", code: otpCode })
+//         .then((verification_check) => console.log(verification_check.status))
+//         .then(() => readline.close());
+//     });
+//   });
